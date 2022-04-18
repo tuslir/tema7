c6afd60c99714c0aca4652650e0aea5a5c620316
Sebastian Winnem <sebastianbwinnem@gmail.com>
2022-04-18 14:01:11 +0200

    Introsekvens
    Added Introsekvens til å spillet når man starter spillet

---
+++ Assets/Scripts/PlayerScripts/CollideScript.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class CollideScript : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] private Vector3 sizeIncrease;
+    [SerializeField] private Vector3 sizeDecrease;
+    [SerializeField] private Vector3 camZoom;
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject player;
+    public static int fuel;
+    public static bool isCrashing;
+    RespawnSprite respawn;
+
+    void Start()
+    {
+        fuel = 0;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        respawn = other.gameObject.GetComponent<RespawnSprite>();
+
+
+        if (other.CompareTag("Consumable"))
+        {
+
+            fuel++;
+            //Increases the scale/size of flame with a given vector 3 value
+            this.gameObject.transform.localScale = this.gameObject.transform.localScale + sizeIncrease;
+            cam.transform.position -= camZoom;
+            //Destroy(other);
+            print(fuel);
+            respawn.BurnDown();
+        }
+
+        if (other.CompareTag("MidConsumable") && PlayerStates.state == PlayerStates.playerLvL.lvl2)
+        {
+            fuel++;
+            //Increases the scale/size of flame with a given vector 3 value
+            this.gameObject.transform.localScale = this.gameObject.transform.localScale + sizeIncrease;
+            cam.transform.position -= camZoom;
+            //Destroy(other);
+            print(fuel);
+            respawn.BurnDown();
+        }
+
+        if (other.CompareTag("Hazard")&& fuel >=1)
+        {
+            fuel--;
+            //Shrinks Player size
+            this.gameObject.transform.localScale = this.gameObject.transform.localScale - sizeDecrease;
+            cam.transform.position += camZoom;
+            Debug.Log(fuel);
+            Destroy(other);
+            //if (fuel >= 1)
+            //{
+            //}
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("MidConsumable") && PlayerStates.state == PlayerStates.playerLvL.lvl1)
+        {
+            isCrashing = true;
+            PlayerAnimationController.Crash();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("MidConsumable") && PlayerStates.state == PlayerStates.playerLvL.lvl1)
+        {
+            isCrashing = false;
+        }
+
+    }
+}
